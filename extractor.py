303a0ecdaeee8dbe47d90cf3766be112fd32dbb7
Steven Laan <TheRealNeojume@gmail.com>
2012-01-11 18:21:03 +0100

    Extractor doet dingen

--- extractor.py
+++ extractor.py
@@ -1,18 +1,57 @@
 # Extractor extracts a column from a csv file
 import csv
+import nltk
 
+# Open a file
 file = csv.reader(open('DataCSV.csv', 'rb'), delimiter=',', quotechar='"')
 
-
-attributes = []
+sentence = {}
+sentiment = {}
 i = 0
 
-for row in file:
+# Collect sentences and sentiments
+for entry in file:
+    # Do not include the header!
+    if i == 0:
+        i += 1
+        continue
     
-    attributes.append( row[9] )
-
-    # Stop at 20
+    # The actual message is the 9th attribute, sentiment is the 4th
+    sentence[i - 1] = entry[9]
+    sentiment[i - 1] = float(entry[4])
+    
+    # Stop at 20 (later remove this)
     i += 1
-    if ( i > 20 ): break
+    if ( i > 200 ): break
 
-print '\n'.join(attributes)
+number_of_items = i - 1; # -1 because of header
+
+# Create corpus and count word frequencies
+corpus = {}
+
+for i in range(number_of_items):
+    # Tokenize the sentence
+    tk_sentence = nltk.tokenize.word_tokenize( sentence[i] )
+
+    # Iterate over every token
+    for token in tk_sentence:
+        if token in corpus.keys():
+            # Check for sentiment
+            if sentiment[i] != 0:
+                corpus[token] = corpus[token][0] + 1, corpus[token][1] + 1
+            else:
+                corpus[token] = corpus[token][0] + 1, 0
+        else:
+            # Check for sentiment
+            if sentiment[i] != 0:
+                corpus[token] = 1, 1
+            else:
+                corpus[token] = 1, 0
+
+# Corpus created, calculate frequencies
+frequency = {}
+for token in corpus.keys():
+    frequency[token] = float(corpus[token][1]) / corpus[token][0]
+    print token, ' || ',corpus[token][1],' / ',corpus[token][0],' = ', frequency[token], '\n'
+
+                
