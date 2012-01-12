e20d34531b63feff62ae01561b3daabef939e7c6
Auke Wiggers <wiggers.auke@gmail.com>
2012-01-12 16:44:31 +0100

    Divided extractor into classes, now the main method (rename perhaps?)

--- extractor.py
+++ extractor.py
@@ -2,85 +2,109 @@
 import csv
 import nltk
 
-# Open a file
-file = csv.reader(open('DataCSV.csv', 'rb'), delimiter=',', quotechar='"')
+class Main():
 
-sentence = {}
-sentiment = {}
+    # Open a file
+    file1 = csv.reader(open('DataCSV.csv', 'rb'), delimiter=',', quotechar='"')     
 
-# do not include header!
-i = 0
+    # used dictionaries, with enlarged scope.
+    sentence = {}
+    sentiment = {}
+    corpus = {}
+    probWord = {}
+    probSent = {}
+    confusion = {}
 
-# Collect sentences and sentiments
-for entry in file:
-    if i == 0:
-        i+=1
-        continue
-    
-    # The actual message is the 9th attribute, sentiment is the 4th
-    sentence[i - 1] = entry[9]
-    sentiment[i - 1] = float(entry[4])
-    
-    # Stop at 20 (later remove this)
-    i += 1
-    if ( i == 9999 ): break
+    def __init__(self):
+        self.makeCorpus()
+        self.calcProbability()
+        self.printResults()
+        
+    def makeCorpus(self):
+        self.sentence = {}
+        self.sentiment = {}
 
-number_of_items = i - 1; # -1 because of header  (== len(sentence))
+        # do not include header!
+        i = 0
 
-# Create corpus and count word frequencies
-corpus = {}
+        # Collect sentences and sentiments
+        for entry in self.file1:
+            if i == 0:
+                i+=1
+                continue
+            
+            # The actual message is the 9th attribute, sentiment is the 4th
+            self.sentence[i - 1] = entry[9]
+            self.sentiment[i - 1] = float(entry[4])
+            
+            # Stop at 20 (later remove this)
+            i += 1
+            if ( i == 9999 ): break
 
-for i in range(number_of_items):
-    # Tokenize the sentence
-    tk_sentence = nltk.tokenize.word_tokenize( sentence[i] )
+        number_of_items = i - 1; # -1 because of header  (== len(sentence))
 
-    # Iterate over every token
-    for token in tk_sentence:
-        if token in corpus:
-            # Check for sentiment
-            if sentiment[i] != 0:
-                corpus[token] = corpus[token][0] + 1, corpus[token][1] + 1
-            else:
-                corpus[token] = corpus[token][0] + 1, corpus[token][1]
-        else:
-            # Check for sentiment
-            if sentiment[i] != 0:
-                corpus[token] = 1, 1
-            else:
-                corpus[token] = 1, 0
+        # Create corpus and count word frequencies
+        self.corpus = {}
 
-# Corpus created, calculate words probability of sentiment based on frequency
-probWord = {}
-for token in corpus.keys():
-    probWord[token] = float(corpus[token][1]) / corpus[token][0]
-    # print token, ' || ',corpus[token][1],' / ',corpus[token][0],' = ', probWord[token], '\n'
+        for i in range(number_of_items):
+            # Tokenize the sentence
+            tk_sentence = nltk.tokenize.word_tokenize( self.sentence[i] )
 
-# Probability of sentiment per word calculated, estimate sentence probability of sentiment
-probSent = {}
-confusion = {}
-confusion["tp"] = 0
-confusion["tn"] = 0
-confusion["fp"] = 0
-confusion["fn"] = 0
+            # Iterate over every token
+            for token in tk_sentence:
+                if token in self.corpus:
+                    # Check for sentiment
+                    if self.sentiment[i] != 0:
+                        self.corpus[token] = self.corpus[token][0] + 1, self.corpus[token][1] + 1
+                    else:
+                        self.corpus[token] = self.corpus[token][0] + 1, self.corpus[token][1]
+                else:
+                    # Check for sentiment
+                    if self.sentiment[i] != 0:
+                        self.corpus[token] = 1, 1
+                    else:
+                        self.corpus[token] = 1, 0
 
-for i in range(len(sentence)):
-        p = 1
-        tk_sent = nltk.tokenize.word_tokenize( sentence[i] )
-        for token in tk_sent:
-            p = p + probWord[token]
-        probSent[i] = p / float(len(tk_sent)) # to be extra certain intdiv does not occur
-        if probSent[i] > 0.31:
-            if sentiment[i] == 0:
-                confusion["fp"] += 1
-            else:
-                confusion["tp"] += 1
-        if probSent[i] < 0.31:
-            if sentiment[i] == 0:
-                confusion["tn"] += 1
-            else:
-                confusion["fn"] += 1
-        #print i, 'PROB', probSent[i], 'SENT', sentiment[i]
-    
-print confusion
-print 'accuracy = ', float(confusion["tp"] + confusion["tn"]) / (confusion["tp"] + confusion["tn"] + confusion["fp"] + confusion["fn"])
-print 'precision = ', float(confusion["tp"]) / (confusion["tp"] + confusion["fp"] )
+    def calcProbability(self):
+        # Corpus created, calculate words probability of sentiment based on frequency
+        self.probWord = {}
+        for token in self.corpus.keys():
+            self.probWord[token] = float(self.corpus[token][1]) / self.corpus[token][0]
+            # print token, ' || ',corpus[token][1],' / ',corpus[token][0],' = ', probWord[token], '\n'
+
+        # Probability of sentiment per word calculated, estimate sentence probability of sentiment
+        self.probSent = {}
+        self.confusion = {}
+        self.confusion["tp"] = 0
+        self.confusion["tn"] = 0
+        self.confusion["fp"] = 0
+        self.confusion["fn"] = 0
+
+        for i in range(len(self.sentence)):
+                p = 1
+                tk_sent = nltk.tokenize.word_tokenize( self.sentence[i] )
+                for token in tk_sent:
+                    p = p + self.probWord[token]
+                self.probSent[i] = p / float(len(tk_sent)) # to be extra certain intdiv does not occur
+                if self.probSent[i] > 0.31:
+                    if self.sentiment[i] == 0:
+                        self.confusion["fp"] += 1
+                    else:
+                        self.confusion["tp"] += 1
+                if self.probSent[i] < 0.31:
+                    if self.sentiment[i] == 0:
+                        self.confusion["tn"] += 1
+                    else:
+                        self.confusion["fn"] += 1
+                #print i, 'PROB', self.probSent[i], 'SENT', self.sentiment[i]
+
+    def percept(self):
+        self.probSent[]
+        self.sentiment[]
+
+    def printResults(self):        
+        print self.confusion
+        print 'accuracy = ', float(self.confusion["tp"] + self.confusion["tn"]) / (self.confusion["tp"] + self.confusion["tn"] + self.confusion["fp"] + self.confusion["fn"])
+        print 'precision = ', float(self.confusion["tp"]) / (self.confusion["tp"] + self.confusion["fp"] )
+
+
