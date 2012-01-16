35860a6100ff3e5813a0f997b1ce17c67c8ee6ba
Steven Laan <TheRealNeojume@gmail.com>
2012-01-16 17:47:46 +0100

    Added method to change weights

--- perceptron.py
+++ perceptron.py
@@ -42,7 +42,6 @@ class Perceptron(object):
         return n
     
     def set_weights(self,new_weights):
-		# Set the weights to a given list
         self.weights = new_weights
 
     def reset(self):
