2e481f58f18c9c03fc8a56cd2de55990abdd8b58
Camiel Verschoor <verschoor@uva.nl>
2012-01-16 23:45:52 +0100

    Logistic Regression added

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,7 +1,6 @@
 # Implements Logistic Regression
 import math
 
-
 class LogisticRegression(object):
     def __init__(self, theta = [0.5, 0.5], alpha = 0.05):
         self.theta = theta;
@@ -10,14 +9,16 @@ class LogisticRegression(object):
     def _h(self, theta, x):
         result = list()
         for i in range(len(theta)):
-            result.append(theta(i) * x(i))
+            result.append( theta[i] * x[i] )
         return 1 / float( 1 + math.exp( -sum(result) ) )
 
-    def _costFunction(self, theta, x, y):
-        cost = -1/len(x);
+    def _costFunction(self, theta, samples):
+        cost = -1/len(samples);
         temp = 0;
-        for i in range(len(x)):
-            temp += ( y[i] * math.log( h( theta, x[i] ) ) + ( 1 - y[i] ) * math.log( 1 - h( theta, x[i] ) ) )
+        for i in range(len(samples)):
+            x = [ 1, samples[i][0] ]
+            y = samples[i][1]
+            temp += ( y * math.log( self._h( theta, x ) ) + ( 1 - y ) * math.log( 1 - self._h( theta, x ) ) )
         return cost * temp
 
     def _gradientDescent(self, theta, samples, a):
@@ -25,12 +26,14 @@ class LogisticRegression(object):
         for i in range(len(theta)):
             temp = 0
             for j in range(len(samples)):
-                
-                temp += ( h( theta, (samples[j][0] ) - y[j] ) * x[j][i]
-             newTheta[i] = theta[i] - a * temp
+                x = [ 1, samples[j][0] ]
+                y = samples[j][1]
+                temp += ( self._h( theta, x ) - y ) * x[i]
+            newTheta[i] = theta[i] - a * temp
         return newTheta
     
     def train(self, samples):
         for i in range(1000):
-            self.theta = _gradientDescent(self.theta, samples, self.alpha)
+            self.theta = self._gradientDescent(self.theta, samples, self.alpha)
+            print "Cost: ", self._costFunction(self.theta, samples)
         return self.theta
